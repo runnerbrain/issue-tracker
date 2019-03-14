3f2a980732d7987c8da5566c490c838ba31d9b1d
runnerbrain <runnerbrain@gmail.com>
2019-03-14 11:09:25 -0400

    updated .travis.yml. still getting deployment problems

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', function(request, response){
-    response.sendfile('index.html');
+    response.sendFile('index.html');
 });
+
+app.get('/issues', (request,response) => {
+    response.sendFile(path.join(__dirname,'/public','issues.html'));
+})
+
+app.get('/issues',(request,response) => {
+    
+})
 
 app.listen(process.env.PORT || 8080);
 
