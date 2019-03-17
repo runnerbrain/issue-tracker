2430bd091fbaab23c8c630c61c88a7c81637e017
runnerbrain <runnerbrain@gmail.com>
2019-03-17 11:36:52 -0400

    did some perliminary css design

--- server.js
+++ server.js
@@ -12,9 +12,22 @@ app.get('/issues', (request,response) => {
     response.sendFile(path.join(__dirname,'/public','issues.html'));
 })
 
-app.get('/issues',(request,response) => {
-    
-})
+app.get('/issues',(req,res) => {})
+app.get('/issues/:due',(req,res)=>{});
+app.get('/issues/:category',(req,res)=>{});
+app.get('/issues/:due/:category',(req,res)=>{});
+app.get('/issues/:issue_id/comments',(req,res)=>{});
+
+app.post('/issues',(req,res)=>{});
+app.post('/issues/:issue_id/comments',(req,res)=>{});
+
+app.put('issues/:issue_id',(req,res)=>{});
+app.put('/issues/:issue_id/:comment_id',(req,res)=>{});
+
+app.delete('/issues/:issue_id',(req,res)=>{});
+app.delete('/issues/:issue_id/:comment_id/',(req,res)=>{})
+
+
 
 app.listen(process.env.PORT || 8080);
 
